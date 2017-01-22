5a8d450eaf19d0433dcc0e2ba96e68e503d2c5ef
srilatha chappidi <srilatha chappidi>
2017-01-21 22:14:25 -0700

    week2 homework

---
+++ create_blog_authors/src/main/java/course/dto/User.java
@@ -0,0 +1,34 @@
+package course.dto;
+
+/**
+ * Created by Srilatha on 1/21/2017.
+ */
+public class User {
+    private String userName;
+    private String passWord;
+    private String email;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
